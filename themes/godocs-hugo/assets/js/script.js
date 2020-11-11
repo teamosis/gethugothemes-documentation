6896cc0bef1cf0b54e08e7caa247a529fa17b10b
somratpro <abdulmonnafsomrat@gmail.com>
2020-11-11 12:51:21 +0600

    optimised speed

--- themes/godocs-hugo/assets/js/script.js
+++ themes/godocs-hugo/assets/js/script.js
@@ -1,7 +1,9 @@
-// Preloader js    
-$(window).on('load', function () {
-	$('.preloader').fadeOut(100);
-});
+// Preloader js  
+function preloader() {
+	$('.preloader').delay(100).fadeOut(10);
+}
+$(preloader);
+$(document).on("turbolinks:load", preloader);
 
 (function ($) {
 	'use strict';
