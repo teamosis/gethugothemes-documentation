6d7bae38efd4c1c0457b3eab248875569d90ff6b
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-01-12 11:43:46 +0700

    chore: adding some dev helpers
    - copy bin/replacements.template.sh to bin/replacements.sh and edit local paths to modules
    - run ./bin/local-dev-up.sh to add replacements to go.mod
    - run .bin/local-dev-down.sh to remove replacements from go.mod
    
    It probably makes sense to add git hooks that do this automatically in the future.

---
+++ bin/replacements.template.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+declare -a REPLACE=(
+  '/home/patrick/Projects/dnb-hugo/debugprint'
+  '/home/patrick/Projects/dnb-hugo/components/functions'
+  '/home/patrick/Projects/dnb-hugo/components/netlification'
+  '/home/patrick/Projects/dnb-hugo/libraries/bootstrap5'
+  '/home/patrick/Projects/ThemeFisher/core-docs'
+)
