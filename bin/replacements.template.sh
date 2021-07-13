0750edd43407441f443d6c448573b7b1c5dbc441
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-07-13 21:01:54 +0700

    chore: move from dnb-hugo to dnb-org organisation

--- bin/replacements.template.sh
+++ bin/replacements.template.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 declare -a REPLACE=(
-  '/home/patrick/Projects/dnb-hugo/debugprint'
-  '/home/patrick/Projects/dnb-hugo/components/functions'
-  '/home/patrick/Projects/dnb-hugo/components/netlification'
-  '/home/patrick/Projects/dnb-hugo/libraries/bootstrap5'
+  '/home/patrick/Projects/dnb-org/debugprint'
+  '/home/patrick/Projects/dnb-org/components/functions'
+  '/home/patrick/Projects/dnb-org/components/netlification'
+  '/home/patrick/Projects/dnb-org/libraries/bootstrap5'
   '/home/patrick/Projects/ThemeFisher/core-docs'
 )
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/gethugothemes/documentation
 go 1.15
 
 require (
-	github.com/dnb-hugo/components/netlification v1.1.14 // indirect
+	github.com/dnb-org/components/netlification v1.2.0 // indirect
 	github.com/themefisher/core-docs v1.0.43 // indirect
 )
